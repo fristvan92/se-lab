cec05a8e2776c68a0bd07d5388f0b25e4798af03
unknown <190262@bme.hu>
2025-10-08 17:13:50 +0200

    Fixed what the reviwer said.

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -15,11 +15,10 @@ public class TorpedoStore {
   private int torpedoCount = 0;
 
   //Random generator for simulating random values
-  private Random generator;
+  private static Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
-    generator= new Random();
 
     // update failure rate if it was specified in an environment variable
     String failureEnv = System.getenv("IVT_RATE");
